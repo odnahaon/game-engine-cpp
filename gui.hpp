aae91a17f31c2752450d58dc8c638a5bc846ba79
Hypr_Nova <noah.paul.ando@gmail.com>
2022-02-10 00:12:53 -0800

    Revert "gui stuff"
    This reverts commit 12e4abe0a272429d342a048f0c555cc6ee98d1ef.

--- gui.hpp
+++
@@ -1,7 +0,0 @@
-#ifndef GUI_HPP
-#define GUI_HPP
-
-void button(double xPos, double yPos, double xSize, double ySize, char text);
-void slider(double xPos, double yPos, double xSize, double ySize, char text, double minValue, double maxValue);
-
-#endif
