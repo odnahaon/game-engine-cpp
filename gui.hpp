12e4abe0a272429d342a048f0c555cc6ee98d1ef
Hypr_Nova <noah.paul.ando@gmail.com>
2022-02-10 00:12:27 -0800

    gui stuff

---
+++ gui.hpp
@@ -0,0 +1,7 @@
+#ifndef GUI_HPP
+#define GUI_HPP
+
+void button(double xPos, double yPos, double xSize, double ySize, char text);
+void slider(double xPos, double yPos, double xSize, double ySize, char text, double minValue, double maxValue);
+
+#endif
