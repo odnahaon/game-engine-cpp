cb9d4ae04061c50ebb7d0c7a5b33251a590b57c8
noah-ando <noah.paul.ando@gmail.com>
2022-02-24 13:12:37 -0800

    Error messages

--- loadTextures.cpp
+++ loadTextures.cpp
@@ -17,15 +17,29 @@ unsigned int loadCubemap(std::vector<std::string> faces) {
     glBindTexture(GL_TEXTURE_CUBE_MAP, textureID);
 
     int width, height, nrChannels;
+    int errorCount = 0;
+    int errorMax = 1024;
+    bool e = false;
     for (unsigned int i = 0; i < faces.size(); i++) {
+        int check = stbi_info(faces[i].c_str(), &width, &height, &nrChannels);
+        if (check == 0 && errorCount <= errorMax) {
+            std::cout << "Image info failed to load at path: " << faces[i] << ".\n" << std::endl;
+            errorCount++;
+        }
         unsigned char* data = stbi_load(faces[i].c_str(), &width, &height, &nrChannels, 0);
         if (data) {
             glTexImage2D(GL_TEXTURE_CUBE_MAP_POSITIVE_X + i, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
             stbi_image_free(data);
-        }
-        else {
-            std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+        } else {
+            if (errorCount <= errorMax) {
+                std::cout << "Cubemap tex failed to load at path: " << faces[i] << std::endl;
+                errorCount++;
+            }
             stbi_image_free(data);
+        }
+        if (errorCount >= errorMax && e != true) {
+            e = true;
+            std::cout << "Error max reached. Max: " << errorMax << std::endl;
         }
     }
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
